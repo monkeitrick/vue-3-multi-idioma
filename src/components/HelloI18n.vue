6697e9ce9ec725233acd3f8420b1c5805bd8abd7
SniperMonkey <93905119+monkeitrick@users.noreply.github.com>
2023-02-07 11:44:04 +0100

    Initial commit

---
+++ src/components/HelloI18n.vue
@@ -0,0 +1,33 @@
+<template>
+  <h1>{{ $t("title") }}</h1>
+  <p>{{ $t("description") }}</p>
+  <LocaleSwitcher/>
+</template>
+
+<script>
+// import { defineComponent } from 'vue'
+// import { useI18n } from 'vue-i18n'
+import LocaleSwitcher from './LocaleSwitcher.vue'; 
+export default{
+  name: 'HelloI18n',
+  components: {LocaleSwitcherVue}
+  // setup
+  //   const  t  = useI18n
+  //     inheritLocale: true,
+  //     useScope: 'local'
+  //   
+
+  //   // Something todo ..
+
+  //   return  t 
+  // 
+};
+</script>
+
+<i18n>
+{
+  "en": {
+    "hello": "Hello i18n in SFC!"
+  }
+}
+</i18n>
